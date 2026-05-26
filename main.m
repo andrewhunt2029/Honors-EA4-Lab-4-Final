8b711a8afb684b6202c010fdfa08b21bd492f152
Andrew Hunt <andrewhunt2029@u.northwestern.edu>
2026-05-26 15:11:55 -0500

    continued building main driver

--- main.m
+++ main.m
@@ -24,7 +24,7 @@ N = 2;
 omega1 = 20;
 omega2 = 10;
 K = 5;
-[t, theta, thetap] = simulate_oscillations(N, initvec, [omega1, omega2])
+[t, theta, thetap] = simulate_oscillations(N, initvec, [omega1, omega2]);
 % Plot results
 figure('Name', 'Oscillations | N = 2, diff omega > K');
 plot(t, theta(1, :), 'k-',  'LineWidth', 2,   'DisplayName', '1st oscillator'); hold on;
@@ -37,7 +37,7 @@ legend; grid on;
 omega1 = 20;
 omega2 = 10;
 K = 5;
-[t, theta, thetap] = simulate_oscillations(N, initvec, [omega1, omega2])
+[t, theta, thetap] = simulate_oscillations(N, initvec, [omega1, omega2]);
 figure('Name', 'Oscillations | N = 2, diff omega < K');
 plot(t, theta(1, :), 'k-',  'LineWidth', 2,   'DisplayName', '1st oscillator'); hold on;
 plot(t, theta(2, :),  'r--', 'LineWidth', 2, 'DisplayName', '2nd oscillator');
@@ -50,20 +50,37 @@ legend; grid on;
 
 % ----- Part 3: N >> 1 Case, varying K -----
 
-for K = 0:0.01:1 % coupling strengths vector
+N = 50; % number of oscillators
+Kvals = 0:0.01:1;
+R = zeros(1, length(Kvals)); % vector of order parameter... "center of mass distances" / "proximity to synchronization"
+
+for K = Kvals % cycle through uniform range of coupling strengths
     
-    % Define initial conditions
-    N = 50; % number of oscillators
-    initvec = zeros(1, N); % initial theta vals vector
-    omegavec = zeros(1, N); % initial omega vals vector
+    % Define random initial conditions
+    initvec = 2*pi*rand(1, N); % initial theta vals vector, random from uniform distribution 0 to 2pi
+    omegavec = randn(1, N) + 1; % initial omega vals vector, random from Gaussian curve
 
     % run integration of equation, where theta = phase
     [t, theta, thetap] = simulate_oscillations(N, initvec, omegavec);
 
     % theta, thetap are matrices of size [t, N]
+
+    % Find R
+
+    %R(find(Kvals == K)) = (1/N)*sum(e^)
 end
 
+% Calculate threshold coupling strength, Kc
 
+Kc = (FILL IN)*sqrt(2/pi);
+
+% Plot R vals with respect to K vals
+figure('Name', 'R(K) | N = %d', N);
+plot(K, R, 'k-',  'LineWidth', 2,   'DisplayName', 'R(K)'); hold on;
+xline(Kc,'-',{'Threshold Coupling Strength','Kc'});
+xlabel('Coupling Strength K'); ylabel('Order Parameter R');
+title('Order Parameter R as a function of K | N = %d', N);
+legend; grid on;
 
 % EXTRA CREDIT 1: movie
 
