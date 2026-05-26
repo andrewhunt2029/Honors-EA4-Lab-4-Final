4922c010568e7a3515bcdaa88066257afb38a7bb
Andrew Hunt <andrewhunt2029@u.northwestern.edu>
2026-05-26 16:57:46 -0500

    fixed code according to updated simulation function

--- main.m
+++ main.m
@@ -2,33 +2,20 @@
 
 clear; clc; close all;
 
-% ----- Part 1: example of how to integrate phase system -----
-
-% Define initial conditions
-
-K = 1; % coupling strength
-N = 50; % number of oscillators
-initvec = zeros(1, N); % initial theta vals vector
-omegavec = zeros(1, N); % initial omega vals vector
-
-% run integration of equation, where theta = phase
-[t, theta, thetap] = simulate_oscillations(N, initvec, omegavec);
-
-% theta, thetap are matrices of size [t, N]
-
-% ----- Part 2: N = 2 case -----
+% ----- Part 1: N = 2 case -----
 
 N = 2;
 
 % Case 1: diff omega > K
 omega1 = 20;
 omega2 = 10;
+initvec = [0, 2.5]; % initial theta vals
 K = 5;
-[t, theta, thetap] = simulate_oscillations(N, initvec, [omega1, omega2]);
+[t, theta] = simulate_oscillations(N, K, initvec, [omega1, omega2]);
 % Plot results
 figure('Name', 'Oscillations | N = 2, diff omega > K');
-plot(t, theta(1, :), 'k-',  'LineWidth', 2,   'DisplayName', '1st oscillator'); hold on;
-plot(t, theta(2, :),  'r--', 'LineWidth', 2, 'DisplayName', '2nd oscillator');
+plot(t, theta(:, 1), 'k-',  'LineWidth', 2,   'DisplayName', '1st oscillator'); hold on;
+plot(t, theta(:, 2),  'r--', 'LineWidth', 2, 'DisplayName', '2nd oscillator');
 xlabel('\omega'); ylabel('Response Amplitude R');
 title('Oscillations | N = 2, diff omega > K');
 legend; grid on;
@@ -37,10 +24,10 @@ legend; grid on;
 omega1 = 20;
 omega2 = 10;
 K = 5;
-[t, theta, thetap] = simulate_oscillations(N, initvec, [omega1, omega2]);
+[t, theta] = simulate_oscillations(N, initvec, [omega1, omega2]);
 figure('Name', 'Oscillations | N = 2, diff omega < K');
-plot(t, theta(1, :), 'k-',  'LineWidth', 2,   'DisplayName', '1st oscillator'); hold on;
-plot(t, theta(2, :),  'r--', 'LineWidth', 2, 'DisplayName', '2nd oscillator');
+plot(t, theta(:, 1), 'k-',  'LineWidth', 2,   'DisplayName', '1st oscillator'); hold on;
+plot(t, theta(:, 2),  'r--', 'LineWidth', 2, 'DisplayName', '2nd oscillator');
 xlabel('\omega'); ylabel('Response Amplitude R');
 title('Oscillations | N = 2, diff omega < K');
 legend; grid on;
@@ -48,7 +35,7 @@ legend; grid on;
 % expected result from mathematical analysis: no fixed points for Case 1 (not enough coupling
 % strength_; fixed points at arcsin(diffomega/K) and pi - arcsin(diffomega/K) 
 
-% ----- Part 3: N >> 1 Case, varying K -----
+% ----- Part 2: N >> 1 Case, varying K -----
 
 N = 50; % number of oscillators
 Kvals = 0:0.01:1;
@@ -61,18 +48,17 @@ for K = Kvals % cycle through uniform range of coupling strengths
     omegavec = randn(1, N) + 1; % initial omega vals vector, random from Gaussian curve
 
     % run integration of equation, where theta = phase
-    [t, theta, thetap] = simulate_oscillations(N, initvec, omegavec);
+    [t, theta] = simulate_oscillations(N, initvec, omegavec);
 
     % theta, thetap are matrices of size [t, N]
 
     % Find R
-
-    %R(find(Kvals == K)) = (1/N)*sum(e^)
+    R(Kvals == K) = (1/N)*sqrt(sum(cos(theta(end, :)))^2 + (sum(sin(theta(end, :)))^2));
 end
 
 % Calculate threshold coupling strength, Kc
 
-Kc = (FILL IN)*sqrt(2/pi);
+%Kc = (FILL IN)*sqrt(2/pi);
 
 % Plot R vals with respect to K vals
 figure('Name', 'R(K) | N = %d', N);
