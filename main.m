fa70633a72f11a8250f1b2d1b83c212d3d8419af
Andrew Hunt <andrewhunt2029@u.northwestern.edu>
2026-05-27 11:58:38 -0500

    fixed errors, made R look better... takes too long to run tho

--- main.m
+++ main.m
@@ -1,6 +1,6 @@
 %% Lab 4: Synchronization - Main Driver Script
 
-clear; clc; close all; rng;
+clear; clc; close all; rng(1);
 
 % ----- Part 1: N = 2 case -----
 
@@ -14,11 +14,13 @@ K = 0.5;
 % theta is matrix of size [t, N]
 % Plot results
 figure('Name', 'Oscillations | N = 2, diff omega > K');
-plot(t, theta(:, 1), 'b-',  'LineWidth', 2,   'DisplayName', '1st oscillator'); hold on;
-plot(t, theta(:, 2),  'r-', 'LineWidth', 2, 'DisplayName', '2nd oscillator');
+plot(t, theta(:, 1), 'b-',  'LineWidth', 1,   'DisplayName', '1st oscillator'); hold on;
+plot(t, theta(:, 2),  'r-', 'LineWidth', 1, 'DisplayName', '2nd oscillator');
 xlabel('iterations t'); ylabel('phase angle (radians)');
 title('Oscillations | N = 2, diff omega > K');
 legend; grid on;
+
+fprintf("N = 2, Case 1 done\n");
 
 % Case 2: diff omega < K
 omegavec = [1, 1.2];
@@ -27,20 +29,22 @@ K = 0.5;
 [t, theta] = simulate_oscillations(N, K, initvec, omegavec);
 % Plot results
 figure('Name', 'Oscillations | N = 2, diff omega < K');
-plot(t, theta(:, 1), 'b-',  'LineWidth', 2,   'DisplayName', '1st oscillator'); hold on;
-plot(t, theta(:, 2),  'r-', 'LineWidth', 2, 'DisplayName', '2nd oscillator');
+plot(t, theta(:, 1), 'b-',  'LineWidth', 1,   'DisplayName', '1st oscillator'); hold on;
+plot(t, theta(:, 2),  'r-', 'LineWidth', 1, 'DisplayName', '2nd oscillator');
 xlabel('iterations t'); ylabel('phase angle (radians)');
 title('Oscillations | N = 2, diff omega < K');
 legend; grid on;
+
+fprintf("N = 2, Case 2 done\n");
 
 % expected result from mathematical analysis: no fixed points for Case 1 (not enough coupling
 % strength); for Case 2, fixed points at arcsin(diffomega/K) and pi - arcsin(diffomega/K) 
 
 % ----- Part 2: N >> 1 Case, varying K -----
 
-N = 50;
+N = 500;
 
-Kvals = 0:0.05:10;
+Kvals = 0:0.02:5;
 R = zeros(1, length(Kvals)); % vector of order parameter... "center of mass distances" / "proximity to synchronization"
 
 for K = Kvals % cycle through uniform range of coupling strengths
@@ -55,8 +59,11 @@ for K = Kvals % cycle through uniform range of coupling strengths
     % theta, thetap are matrices of size [t, N]
 
     % Find R
-    R(Kvals == K) = (1/N)*sqrt(sum(cos(theta(end, :)))^2 + (sum(sin(theta(end, :)))^2));
+    R(Kvals == K) = (1/N)*sqrt(sum(cos(theta(end, :)))^2 + (sum(sin(theta(end, :)))^2)); % <--- should be an equivalent calculation
+    %R(Kvals == K) = (1/N)*abs(sum(exp(1i*theta(end, :))));
 end
+
+fprintf("N >> 1 Case done\n");
 
 % Calculate threshold coupling strength, Kc
 % Kc = the coupling strength such that, if K > Kc, solutions R > 0 exist
@@ -64,14 +71,14 @@ Kc = 2*sqrt(2/pi); % where sigma = 1
 
 % Calculate Kuramoto's Prediction function, the vector Rpredic,
 % with guess proportionality constant a
-a = 2;
-Rpredic = a .* sqrt(K - Kc);
+a = 0.9;
+Rpredic = a .* sqrt(Kvals - Kc);
 
 % Plot R vals with respect to K vals
 figure('Name', sprintf('R(K) | N = %d', N));
-plot(Kvals, R, 'b-',  'LineWidth', 2,   'DisplayName', 'R(K)'); hold on;
+plot(Kvals, R, 'b-',  'LineWidth', 1,   'DisplayName', 'R(K)'); hold on;
 xline(Kc,'-',{'Threshold Coupling Strength'}, 'DisplayName', 'Kc'); hold on;
-plot(Kvals, Rpredic, 'r-','LineWidth', 2,   'DisplayName', 'Kuramoto Prediction');
+plot(Kvals, Rpredic, 'r-','LineWidth', 1,   'DisplayName', 'Kuramoto Prediction');
 xlabel('Coupling Strength K'); ylabel('Order Parameter R');
 title(sprintf('Order Parameter R as a function of K | N = %d', N));
 legend; grid on;
--- simulate_oscillations.m
+++ simulate_oscillations.m
@@ -3,12 +3,15 @@ function [t, theta] = simulate_oscillations(N, K, initvec, omegavec)
  if length(initvec) ~= N || length(omegavec) ~= N
      error('Incorrect vector length');
  end
- if N > 2
+ if N > 2 % MODIFY THIS CASE... it is still just running w/ t=100
      % We are now running the N >> 1 case in the code!
      % Iterate until we reach "steady state", which we interpret as being
      % when R - Rprev is within some tolerance
+    initvec = initvec(:);
+    omegavec = omegavec(:);
+    [t, theta] = ode45(@(t, theta) rhs(t, theta, K/N, omegavec), [0 100], initvec);
  else
-    % We are now running the N > 2 case; simply run 50 iterations
+    % We are now running the N = 2 case; simply run 50 iterations
     initvec = initvec(:);
     omegavec = omegavec(:);
     [t, theta] = ode45(@(t, theta) rhs(t, theta, K/N, omegavec), [0 100], initvec);
