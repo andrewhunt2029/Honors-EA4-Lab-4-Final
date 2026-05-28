83a756913c492d6b35e82ad01a105c944f873db6
Andrew Hunt <andrewhunt2029@u.northwestern.edu>
2026-05-27 22:13:43 -0500

    changed smth small

--- simulate_oscillations.m
+++ simulate_oscillations.m
@@ -3,17 +3,15 @@ function [t, theta] = simulate_oscillations(N, K, initvec, omegavec)
  if length(initvec) ~= N || length(omegavec) ~= N
      error('Incorrect vector length');
  end
+ initvec = initvec(:);
+ omegavec = omegavec(:);
  if N > 2 % MODIFY THIS CASE... it is still just running w/ t=100
      % We are now running the N >> 1 case in the code!
      % Iterate until we reach "steady state", which we interpret as being
      % when R - Rprev is within some tolerance
-    initvec = initvec(:);
-    omegavec = omegavec(:);
     [t, theta] = ode45(@(t, theta) rhs(t, theta, K/N, omegavec), [0 100], initvec);
  else
     % We are now running the N = 2 case; simply run 40 iterations
-    initvec = initvec(:);
-    omegavec = omegavec(:);
     [t, theta] = ode45(@(t, theta) rhs(t, theta, K/N, omegavec), [0 40], initvec);
  end
 end
