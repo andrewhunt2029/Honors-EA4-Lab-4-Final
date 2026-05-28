a2cddcefe0f0bf5c72df43459b90ddd412c55608
Martin Suarez <martinsuarezjaramillo@gmail.com>
2026-05-27 22:22:55 -0500

    Updated simulation Logic

--- simulate_oscillations.m
+++ simulate_oscillations.m
@@ -1,21 +1,16 @@
 % Simulate the phase oscillations for the system of coupled oscillations
-function [t, theta] = simulate_oscillations(N, K, initvec, omegavec)
+function [t, theta] = simulate_oscillations(N, K, initvec, omegavec, tspan)
  if length(initvec) ~= N || length(omegavec) ~= N
      error('Incorrect vector length');
  end
- if N > 2 % MODIFY THIS CASE... it is still just running w/ t=100
+     % MODIFY THIS CASE... it is still just running w/ t=100
      % We are now running the N >> 1 case in the code!
      % Iterate until we reach "steady state", which we interpret as being
      % when R - Rprev is within some tolerance
     initvec = initvec(:);
     omegavec = omegavec(:);
-    [t, theta] = ode45(@(t, theta) rhs(t, theta, K/N, omegavec), [0 100], initvec);
- else
-    % We are now running the N = 2 case; simply run 40 iterations
-    initvec = initvec(:);
-    omegavec = omegavec(:);
-    [t, theta] = ode45(@(t, theta) rhs(t, theta, K/N, omegavec), [0 40], initvec);
- end
+    [t, theta] = ode45(@(t, theta) rhs(t, theta, K/N, omegavec), tspan, initvec);
+
 end
 function dtheta = rhs(t, theta, scale, omegavec)
  dtheta = omegavec + scale*sum(sin(theta' - theta), 2);
